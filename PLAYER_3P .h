8d6bcf1f052d2172a8760bfa3c539c43b37e8b0c
Mathis-C <m.collette@elisa-aerospace.fr>
2024-11-02 22:37:15 +0100

    Update PLAYER_3P .h

--- PLAYER_3P .h
+++ PLAYER_3P .h
@@ -1,38 +1,42 @@
 #ifndef PLAY_3P_H
 #define PLAY_3P_H
-#include "PLATEAU_3P.h"
+#include "plateau_3p.h"
 #include <QMainWindow>
 #include <QWidget>
 #include <QComboBox>
 #include <QPushButton>
 #include <QLabel>
 
+using namespace std;
+
+
 class PLAY_3P : public QWidget
 {
     Q_OBJECT
+
 public:
 
     explicit PLAY_3P(QWidget *parent = nullptr);
+
     void INTERFACE_3P();
-    
+
+
 public slots:
-    
+
     void colonneCliquee(int col);
     void Reinitialiser();
-    
+
 private:
-    
-    
+
+
     QLabel * TITRE;
     QPushButton * RESET;
     QPushButton * Quit1;
-    QPushButton *boutonColonne;
-    PLATEAU_3P *PLATEAU;
-    int joueurActuel;
-    
-    
+    QPushButton * boutonColonne;
+    PLATEAU_3P * PLATEAU;
+    int joueurActuel = 1;
     void changerJoueur();
-    
+
 };
 
 
