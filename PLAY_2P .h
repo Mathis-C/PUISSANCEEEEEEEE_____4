dc678ef1be85cc1b8ae7a3c90491d8da269e7c9f
Mathis-C <m.collette@elisa-aerospace.fr>
2024-11-02 09:19:25 +0100

    Update PLAY_2P .h

--- PLAY_2P .h
+++ PLAY_2P .h
@@ -1,10 +1,13 @@
 #ifndef PLAY_2P_H
 #define PLAY_2P_H
+#include "PLATEAU_2P.h"
 #include <QMainWindow>
 #include <QWidget>
 #include <QComboBox>
 #include <QPushButton>
 #include <QLabel>
+
+using namespace std;
 
 class PLAY_2P : public QWidget
 {
@@ -13,14 +16,24 @@ class PLAY_2P : public QWidget
 public:
 
     explicit PLAY_2P(QWidget *parent = nullptr);
+
     void INTERFACE_2P();
+    void colonneCliquee(int col);
+    void reinitialiser();
+
+
 
 
 private:
 
+
     QLabel * TITRE;
     QPushButton * RESET;
     QPushButton * Quit1;
+    PLATEAU_2P *PLATEAU;
+    int joueurActuel;
+
+    void changerJoueur();
 
 };
 
