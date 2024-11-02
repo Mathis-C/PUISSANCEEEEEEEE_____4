985180afb30680c4f3b161759e4d614a90566cc6
Mathis-C <m.collette@elisa-aerospace.fr>
2024-11-02 09:19:57 +0100

    Update PLAYER_2P .cpp

--- PLAYER_2P .cpp
+++ PLAYER_2P .cpp
@@ -1,3 +1,5 @@
+#include "PLAY_2P.h"
+#include "PLATEAU_2P.h"
 #include "qobjectdefs.h"
 #include <QMainWindow>
 #include <QWidget>
@@ -5,35 +7,69 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QMessageBox>
 
-PLAY_2P::PLAY_2P(QWidget *parent) : QWidget(parent) {
-}
+
+PLAY_2P::PLAY_2P(QWidget *parent) : QWidget(parent),  PLATEAU(new PLATEAU_2P(this)), joueurActuel(1) {}
+
 
 void PLAY_2P:: INTERFACE_2P(){
 
-    TITRE = new QLabel (" POWER 4 ", this);
-    RESET = new QPushButton (" RESET ", this);
-    Quit1 = new QPushButton ( " QUIT " , this);
+    setWindowTitle("Puissance 4");
 
-    connect(Quit1, SIGNAL(clicked()), this, SLOT(close()));
+    QVBoxLayout *mainLayout = new QVBoxLayout();
+    setLayout(mainLayout);
 
-    QHBoxLayout * titre = new QHBoxLayout();
-    titre -> addStretch();
-    titre -> addWidget(TITRE);
-    titre -> addStretch();
+    // Titre
+    TITRE = new QLabel("POWER 4", this);
+    QHBoxLayout *titreLayout = new QHBoxLayout();
+    titreLayout->addStretch();
+    titreLayout->addWidget(TITRE);
+    titreLayout->addStretch();
+    mainLayout->addLayout(titreLayout);
 
-    QHBoxLayout * RQ = new QHBoxLayout();
-    RQ -> addWidget(RESET);
-    RQ -> addWidget(Quit1);
+    // Boutons pour chaque colonne
+    QHBoxLayout *colonneLayout = new QHBoxLayout();
+    for (int col = 0; col < 7; ++col) {
+        QPushButton *boutonColonne = new QPushButton(QString::number(col + 1));
+        boutonColonne->setFixedSize(50, 50);
+        connect(boutonColonne, &QPushButton::clicked, [this, col]() { colonneCliquee(col); });
+        colonneLayout->addWidget(boutonColonne);
+    }
+    mainLayout->addLayout(colonneLayout);
 
+    // Plateau
+    mainLayout->addWidget(PLATEAU);
 
-    QVBoxLayout * JEU = new QVBoxLayout(this);
-    JEU -> addLayout(titre);
-    JEU -> addLayout(RQ);
+    // Boutons Reset et Quit
+    RESET = new QPushButton("RESET", this);
+    Quit1 = new QPushButton("QUIT", this);
+    //connect(RESET, &QPushButton::clicked, this, &PLAY_2P::reinitialiser);
+    connect(Quit1, &QPushButton::clicked, this, &PLAY_2P::close);
 
-
-    setLayout(JEU);
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    buttonLayout->addWidget(RESET);
+    buttonLayout->addWidget(Quit1);
+    mainLayout->addLayout(buttonLayout);
 
 }
+
+void PLAY_2P::colonneCliquee(int col) {
+
+    if (PLATEAU->ajouterJeton(col, joueurActuel)) {
+        QMessageBox::information(this, "Victoire!", QString("Joueur %1 a gagné!").arg(joueurActuel));
+        PLATEAU->reinitialiser();
+    }
+    changerJoueur();
+}
+
+void PLAY_2P::changerJoueur() {
+    joueurActuel = (joueurActuel == 1) ? 2 : 1;
+}
+
+
+
+
+
 
 
