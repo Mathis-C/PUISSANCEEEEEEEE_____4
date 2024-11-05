494d167aeb93de1ce2dd6fc9655f2140c69b48c3
Mathis-C <m.collette@elisa-aerospace.fr>
2024-11-05 18:00:16 +0100

    Update MENU_PLAYER .cpp

--- MENU_PLAYER .cpp
+++ MENU_PLAYER .cpp
@@ -1,6 +1,6 @@
 #include "menu_player.h"
-#include "PLAY_2P.h"
-#include "PLAY_3P.h"
+#include "play_2p.h"
+#include "play_3p.h"
 #include "qobjectdefs.h"
 #include <QMainWindow>
 #include <QWidget>
@@ -15,11 +15,21 @@ MENU_PLAYER::MENU_PLAYER(QWidget *parent) : QWidget(parent) {
 
 void MENU_PLAYER:: start(){
 
+    setWindowTitle("POWER 4");   // Titre de l'interface
+    window()->setStyleSheet("background-color: black;"); // Couleur de fond noire
+    window()->setMinimumSize(1000, 500);
 
     nbrJ = new QLabel (" SELECT NUMBERS OF PLAYERS ", this);
+    nbrJ->setStyleSheet("font-size: 60px; font-weight: bold; color: red;");
+
     TwoP = new QPushButton ("2 PLAYERS" , this);
+    TwoP->setStyleSheet("font-size: 25px; font-weight: bold;background-color: darkblue; color: white;");
+
     ThreeP = new QPushButton (" 3 PLAYERS" , this);
-    Quit1 = new QPushButton ( " QUIT " , this);
+    ThreeP->setStyleSheet("font-size: 25px; font-weight: bold;background-color: darkblue; color: white;");
+
+    Quit1 = new QPushButton ( " RETURN " , this);
+    Quit1->setStyleSheet("font-size: 25px; font-weight: bold;background-color: darkblue; color: white;");
 
 
     connect(Quit1, SIGNAL(clicked()), this, SLOT(close()));
@@ -60,7 +70,6 @@ void MENU_PLAYER:: PL_2P(){
     menu2P -> INTERFACE_2P();
 
 }
-
 
 void MENU_PLAYER:: PL_3P(){
 
