241a792e260aa68e76527328089876e7e9c37f2d
Mathis-C <m.collette@elisa-aerospace.fr>
2024-11-07 11:05:55 +0100

    Update PLATEAU_2P .h

--- PLATEAU_2P .h
+++ PLATEAU_2P .h
@@ -7,23 +7,25 @@
 
 using namespace std;
 
-class PLATEAU_2P : public QWidget {
+class PLATEAU_2P : public QWidget {  // déclaration de la class
 
     Q_OBJECT
 
 public:
 
-    explicit PLATEAU_2P(QWidget *parent = nullptr);
+    explicit PLATEAU_2P(QWidget *parent = nullptr); // Déclaration de notre Widget "Parent"
+
+//Initialisation de nos fonctions, qui permettent la gestion et la mise en place du jeu
     bool ajouterJeton(int col, int joueur);
     bool verifierVictoire(int joueur);
     void reinitialiser();
 
 
 private:
-
+//Initialisation d'un vector à deux dimensions qui contient les jetons, définit avec la classe JETON_2P
     vector<vector<JETON_2P*>> grille;
     QGridLayout *layoutGrille;   // Met les Laytout en grille
-    JETON_2P *JETON;
+    JETON_2P *JETON; // Initialisation de notre class JETON
 };
 
 #endif // PLATEAU_2P_H
